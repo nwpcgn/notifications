cfc80330399dfada2048a9bbcf995fe8038ccea2
nwpcgn <119536313+nwpcgn@users.noreply.github.com>
2025-11-28 15:20:05 +0100

    ua

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
 	import { Stepper, Notifications, log } from '$lib'
+
+	log.addMessage({ message: 'Welcome' })
 </script>
 
 <div class="padded page">
-	<Stepper></Stepper>
+	<Stepper views="[1,2,3,4,5,6,7,8,9]"></Stepper>
 </div>
-<div class="aside"></div>
+<div class="aside">
+	<Notifications></Notifications>
+</div>
